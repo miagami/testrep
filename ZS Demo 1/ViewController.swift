ef4dad7d3503e0f35c8b23f7e202ea08f8fa35e0
Zbigniew Szczerbetka <zszczerbetka@zbigniews-macbook-pro.local>
2017-08-30 15:07:45 +0200

    My commit message

--- ZS Demo 1/ViewController.swift
+++ ZS Demo 1/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func sayHello(_ sender: AnyObject) {
+        label.text = "Dupa"
+        
+    }
 }
 
