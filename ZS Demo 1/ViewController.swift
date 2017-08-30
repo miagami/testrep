eee768ac4ea982acf248d9110a64f90ae74d8761
Zbigniew Szczerbetka <zszczerbetka@zbigniews-macbook-pro.local>
2017-08-30 11:46:06 +0200

    Initial Commit

---
+++ ZS Demo 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZS Demo 1
+//
+//  Created by Zbigniew Szczerbetka on 30/08/2017.
+//  Copyright © 2017 Zbigniew Szczerbetka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
